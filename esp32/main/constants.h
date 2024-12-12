b0c1a9f5fdb30f302a043c8ecd8e7598c6ffcb5d
Deciptikon <deciptikon@mail.ru>
2024-12-12 16:55:42 +0300

    добавлена кнопка сброса логина и пароля, на плате это кнопка BOOT

--- esp32/main/constants.h
+++ esp32/main/constants.h
@@ -3,11 +3,11 @@
 
 const String VERSION = "v2.4";                      // Версия ПО
 
-const String SSID_AP_WIFI = "ESP_AP_v0.2";          // Имя точки доступа
-const String ADDRES_AP_WIFI = "esp32";              // Адрес точки доступа "http://esp32.local"
+const char * SSID_AP_WIFI = "ESP_AP_v0.2";          // Имя точки доступа
+const char * ADDRES_AP_WIFI = "esp32";              // Адрес точки доступа "http://esp32.local"
 
-const String KEY_EEPROM_LOGIN = "Login";            // Ключ для записи и чтения Логина
-const String KEY_EEPROM_PASSWORD = "Password";      // Ключ для записи и чтения Пароля
+const char * KEY_EEPROM_LOGIN = "Login";            // Ключ для записи и чтения Логина
+const char * KEY_EEPROM_PASSWORD = "Password";      // Ключ для записи и чтения Пароля
 
 const int DELAY_TIME = 1000;                        // Время задержки в миллисекундах
 
--- esp32/main/main.ino
+++ esp32/main/main.ino
@@ -9,6 +9,8 @@ Preferences preferences;
 WebServer server(80);
 bool ledState = false;
 const int led = 2;
+const int bt = 0;
+
 String login = "";
 String password = "";
 
@@ -31,7 +33,7 @@ void handleRoot() {
     </body>
     </html>
   )";
-  server.send(200, "text/html", page);
+  server.send(200, "text/html; charset=utf-8", page);
 }
 
 void handleSubmit() {
@@ -47,30 +49,41 @@ void handleSubmit() {
   preferences.putString(KEY_EEPROM_PASSWORD, password);
 
   // Ответ сервера
-  server.send(200, "text/plain", "Данные сохранены");
+  server.send(200, "text/plain; charset=utf-8", "Данные сохранены");
 }
 
 void setup() {
   Serial.begin(115200);
   pinMode(led, OUTPUT);
+  pinMode(bt, INPUT);
 
   digitalWrite(led, HIGH);
-  delay(1000);
+  delay(5000);
   digitalWrite(led, LOW);
 
   if (!preferences.begin("my-app", false)) {
     Serial.println("Ошибка открытия памяти!");
     return;
   }
+  login = preferences.getString(KEY_EEPROM_LOGIN, "");
+  password = preferences.getString(KEY_EEPROM_PASSWORD, "");
+  //preferences.end();
 
   // Если Логин не сохранён, то включаем
   // точку доступа для указания логина и пароля.
   // (проверяем только логин, так как пароль
   // может быть пустым если сохранённая сеть была открытой)
-  login = preferences.getString(KEY_EEPROM_LOGIN, "");
-  if (login == "") { // здесь нужно дописать процедуру сброса логина и пароля
-    setupWiFiAP(SSID_AP_WIFI);
+  if (login == "" || digitalRead(bt) == LOW) { // здесь нужно дописать процедуру сброса логина и пароля
+    digitalWrite(led, HIGH);
+    delay(200);
+    digitalWrite(led, LOW);
+    delay(200);
+    digitalWrite(led, HIGH);
+    delay(200);
+    digitalWrite(led, LOW);
 
+    setupWiFiAP(SSID_AP_WIFI);
+    Serial.println("Точка доступа активирована");
     if (MDNS.begin(ADDRES_AP_WIFI)) {
       Serial.println("mDNS запущен. Адрес http://esp32.local");
     } else {
@@ -92,14 +105,24 @@ void setup() {
     }
 
   } else {//в противном случае,
+    const char* charLogin = login.c_str();
+    const char* charPassword = password.c_str();
     //пытаемся подключится к сети по сохранённым данным
-    password = preferences.getString(KEY_EEPROM_PASSWORD, "");
-    setupWiFi(login, password);
+    Serial.println("Попытка подключится к сохранённой сети...");
+    setupWiFi(charLogin, charPassword);
+    Serial.println("Подключение прошло успешно!");
+
+    while (true) {
+      digitalWrite(led, HIGH);
+      delay(500);
+      digitalWrite(led, LOW);
+      delay(500);
+    }
 
     // !!!!!!! неплохо добавить индикацию миганием светодиодов (или что-то подобное)
   }
 
-  preferences.end();
+
 }
 
 void loop() {
